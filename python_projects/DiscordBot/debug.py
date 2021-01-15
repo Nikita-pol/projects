dc22fac7748e3174c325c9c1cba6c98615b621ae
Nikita-pol <60185455+Nikita-pol@users.noreply.github.com>
2021-01-15 18:52:27 +0300

    Add files via upload

---
+++ python_projects/DiscordBot/debug.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+from config import settings
+
+# bot = commands.Bot(command_prefix=settings['prefix'])
+# @bot.event
+# async def on_ready():
+#     print("Ready")
+#     channel = bot.get_channel(796788095420727316)
+#     await channel.send("Привет, Андрей.")
+#
+#
+# async def on_message(message):
+#    response = requests.get('https://some-random-api.ml/img/fox')
+#    json_data = json.loads(response.text)
+#    embed = discord.Embed(color=0xff9900, title='Вот рандомная лиса.')
+#    embed.set_image(url=json_data['link'])
+#    await message.channel.send(embed=embed)
+# bot.run(settings['token'])
+
+print("hello"[1])
